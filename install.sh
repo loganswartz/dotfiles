e8fe0aa0837b78535cb7f4f0dbab0eb6546d936b
Logan Swartzendruber <logan.swartzendruber@gmail.com>
2020-06-18 10:06:54 -0400

    Fix bugs in install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 echo "Which dotfiles would you like to install?"
 
 for dotfile in $(find "$(dirname $0)" -maxdepth 1 -name ".*" -not -name ".DS_Store" -not -name ".git" -not -name "." -not -name ".."); do
-	dotfile="${dotfile:2}" # trim leading characters
+	dotfile="$(basename $dotfile)"   # trim off preceeding parts of path
 
 	read -p "Install ${dotfile}? " answer
-	if [ "$answer" == "y" ] || [ "$answer" == "Y" ]; then
+	if [ "$answer" == "y" ] || [ "$answer" == "Y" ] || [ "$answer" == "yes" ] || [ "$answer" == "Yes" ]; then
 		# program-specific pre-install steps
 		case $dotfile in
 			*)
@@ -14,18 +14,19 @@ for dotfile in $(find "$(dirname $0)" -maxdepth 1 -name ".*" -not -name ".DS_Sto
 		esac
 
 		# backup existing dotfile
-		if [ -f "$dotfile" ]; then
-			mv ~/$dotfile{,.orig}
+		if [ -f "$HOME/$dotfile" ]; then
+			mv $HOME/$dotfile{,.orig}
 		fi
 
 		# install the dotfile
-		ln -s "$(dirname $0)/$dotfile" "$HOME/$dotfile"
+		dir=$(readlink -m "$(pwd)/$(dirname $0)")
+		ln -s "$dir/$dotfile" "$HOME/$dotfile"
 
 		# program-specific post-install steps
 		case $dotfile in
 			.vimrc)
 				nvim -u "$HOME/.vimrc" +qall
-				nvim -u "$HOME/.vimrc" +PlugInstall +qall
+				nvim +PlugInstall +qall
 				;;
 			*)
 				;;
