5b0efabca0e6941ffbd90f56efacb1fda9727288
Logan Swartzendruber <logan.swartzendruber@gmail.com>
2022-03-04 10:04:54 -0500

    Bootstrap pre-commit setup on all cloned repos

--- install.sh
+++ install.sh
@@ -3,50 +3,52 @@
 echo "Which dotfiles would you like to install?"
 
 resolve() {
-	echo "$(echo "$1" | python3 -c 'import sys; import pathlib; print(pathlib.Path(sys.stdin.read()).expanduser().resolve())')"
+  echo "$(echo "$1" | python3 -c 'import sys; import pathlib; print(pathlib.Path(sys.stdin.read()).expanduser().resolve())')"
 }
 
 install() {
-	dotfile="$1"
-	# backup existing dotfile
-	if [ -f "$HOME/$dotfile" ]; then
-		mv "$HOME/$dotfile{,.orig}"
-	fi
+  dotfile="$1"
+  # backup existing dotfile
+  if [ -f "$HOME/$dotfile" ]; then
+    mv "$HOME/$dotfile{,.orig}"
+  fi
 
-	# install the dotfile
-	#dir=$(readlink -m "$(pwd)/$(dirname $0)")
-	ln -s "$(resolve "$(dirname $0)/$dotfile")" "$HOME/$dotfile"
+  # install the dotfile
+  #dir=$(readlink -m "$(pwd)/$(dirname $0)")
+  ln -s "$(resolve "$(dirname $0)/$dotfile")" "$HOME/$dotfile"
 }
 
 for dotfile in $(find "$(dirname $0)" -maxdepth 1 -name ".*" -not -name ".DS_Store" -not -name ".git" -not -name "." -not -name ".."); do
-	dotfile="$(basename $dotfile)"   # trim off preceeding parts of path
+  dotfile="$(basename $dotfile)"   # trim off preceding parts of path
 
-	read -p "Install ${dotfile}? " answer
-	if [ "$answer" == "y" ] || [ "$answer" == "Y" ] || [ "$answer" == "yes" ] || [ "$answer" == "Yes" ]; then
-		# program-specific pre-install steps
-		case $dotfile in
-			*)
-				;;
-		esac
+  read -p "Install ${dotfile}? " answer
+  if [ "$answer" == "y" ] || [ "$answer" == "Y" ] || [ "$answer" == "yes" ] || [ "$answer" == "Yes" ]; then
+    # program-specific pre-install steps
+    case $dotfile in
+      .gitconfig)
+        ln -s "$dir/git-template" "$HOME/.git-template"
+        ;;
+      *)
+        ;;
+    esac
 
-		install "$dotfile"
+    install "$dotfile"
 
-		# program-specific post-install steps
-		case $dotfile in
-			.vimrc)
-				if command -v nvim; then
-					VIM_EXEC="nvim"
-				else
-					VIM_EXEC="vim"
-				fi
-				# install coc.nvim settings file if installing vimrc
-				ln -s "$dir/coc-settings.json" "$HOME/.config/nvim/coc-settings.json"
-				ln -s "$dir/vim_templates" "$HOME/.config/nvim/templates"
-				$VIM_EXEC -u "$HOME/.vimrc" +qall
-				;;
-			*)
-				;;
-		esac
-	fi
+    # program-specific post-install steps
+    case $dotfile in
+      .vimrc)
+        if command -v nvim; then
+          VIM_EXEC="nvim"
+        else
+          VIM_EXEC="vim"
+        fi
+        # install templates if installing vimrc
+        ln -s "$dir/vim_templates" "$HOME/.config/nvim/templates"
+        $VIM_EXEC -u "$HOME/.vimrc" +qall
+        ;;
+      *)
+        ;;
+    esac
+  fi
 
 done
