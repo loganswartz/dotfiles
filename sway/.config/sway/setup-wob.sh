fedb6d07ae7dd2ff0c513055df03b1f29823964f
Logan Swartzendruber <logan.swartzendruber@gmail.com>
2024-10-04 11:21:56 -0400

    bugfix(sway): Fix some login init issues

---
+++ sway/.config/sway/setup-wob.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+WOBSOCK="${WOBSOCK:-$XDG_RUNTIME_DIR/wob.sock}"
+echo "Attempting to create wob socket at $WOBSOCK..." >&2
+
+killall wob
+rm -f "$WOBSOCK"
+
+if ! mkfifo "$WOBSOCK"; then
+    echo "Failed to create wob socket at $WOBSOCK" >&2
+    exit 1
+fi
+
+tail -f "$WOBSOCK" | wob &
+echo "Created wob socket at $WOBSOCK" >&2
--- sway/.config/sway/update-env.sh
+++ sway/.config/sway/update-env.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-systemctl --user import-environment DISPLAY WAYLAND_DISPLAY SWAYSOCK
+systemctl --user import-environment WAYLAND_DISPLAY DISPLAY XDG_CURRENT_DESKTOP SWAYSOCK I3SOCK XCURSOR_SIZE XCURSOR_THEME
 
 # Import the WAYLAND_DISPLAY env var from sway into the systemd user session.
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
+dbus-update-activation-environment --all XDG_CURRENT_DESKTOP=sway
+
+# Unlock the gnome keyring
+gnome-keyring-daemon --start --components=secrets
 
 # Stop any services that are running, so that they receive the new env var when they restart.
 systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
