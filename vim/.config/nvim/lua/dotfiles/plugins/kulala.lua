10a36eb0cb48dbf5ae73045a960206c43d504efc
Logan Swartzendruber <logan.swartzendruber@gmail.com>
2024-10-25 10:10:35 -0400

    Bugfix sway issues, and add some convenience functions

--- vim/.config/nvim/lua/dotfiles/plugins/kulala.lua
+++ vim/.config/nvim/lua/dotfiles/plugins/kulala.lua
@@ -13,5 +13,18 @@ return {
     keys = {
         { '<leader>e', function() require('kulala').set_selected_env() end },
         { '<C-l>',     function() require('kulala').run() end },
+        {
+            '<C-h>',
+            function()
+                local root = vim.fs.root(0, '.git')
+                local http_dir = vim.fs.joinpath(root, '.http')
+
+                if vim.fn.isdirectory(http_dir) then
+                    vim.cmd.split(http_dir)
+                else
+                    vim.notify('No .http directory found at "' .. http_dir .. '"!', vim.log.levels.WARN)
+                end
+            end,
+        }
     },
 }
